e69730f22de9c33e7bbe7019392b4e5c444012d8
erickmuriithi <rickymurithii@gmail.com>
2022-11-09 16:52:55 +0300

    add files

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
     def self.find_all_ovies_by_year(year)
         Movie.where(year: year)
     end
-
+    
     def self.create_with_title(title)
         Movie.create(title: title)
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2022_11_03_201717) do
 
-    create_table "movies", force: :cascade do |t|
-      t.string "title"
-      t.integer "release_date"
-      t.string "director"
-      t.string "lead"
-      t.boolean "in_theaters"
-    end
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
 end
