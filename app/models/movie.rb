74dbc30e662d13fc5e2e9c65ca3804c2375d2cdc
erickmuriithi <rickymurithii@gmail.com>
2022-11-09 11:45:18 +0300

    add

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,10 @@
 class Movie < ActiveRecord::Base
+    def self.find_all_ovies_by_year(year)
+        Movie.where(year: year)
+    end
+
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
 
 end
