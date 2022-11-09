cac942b801fbc71508e36a051b93a2fcc5dd8116
erickmuriithi <rickymurithii@gmail.com>
2022-11-09 11:46:03 +0300

    add

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,5 +6,16 @@ class Movie < ActiveRecord::Base
     def self.create_with_title(title)
         Movie.create(title: title)
     end
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
 
 end
